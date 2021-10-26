e88677dab0b09f2fa7650314135946a800f740a7
AmarShukla1 <spamfreemenow12345@gmail.com>
2021-10-27 00:16:50 +0530

    basics done

--- logic.js
+++ logic.js
@@ -6,25 +6,96 @@ const player = (name, turn) => {
 
 const gameboard = () => {
   let mygameboard = [];
+  const container=document.querySelector('.container');
   
   for (let i = 0; i < 3; i++) {
-      mygameboard[i]=[];
+    mygameboard[i] = [];
   }
-  for(let i=0;i<3;i++){
+  for (let i = 0; i < 3; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    container.append(row);
     for (let j = 0; j < 3; j++) {
-      mygameboard[i].push(0);
+      const col = document.createElement("div");
+      col.className = "col";
+      col.setAttribute("i", i);
+      col.setAttribute("j", j);
+      row.append(col);
+      mygameboard[i].push(-1);
     }
-}
+  }
   return mygameboard;
 };
 
 first = player("first");
 second = player("second");
 board = gameboard();
-const gamehandler = () => 
-{
+let turn = 1;
+let blocks_filled = 0;
+function checker(k) {
     
+  for (let i = 0; i < 3; i++) {
+    let cnt1=0,cnt2=0;
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] == k) {
+        cnt1++;
+      }
+      if (board[j][i] == k) {
+        cnt2++;
+      }
+    }
+    if (cnt1 == 3 || cnt2 == 3) {
+      return true;
+    }
+  }
+  if (board[0][0] == k && board[1][1] == k && board[2][2] == k) {
+    return true;
+  }
+  if (board[0][2] == k && board[1][1] == k && board[2][0] == k) {
+    return true;
+  }
+  return false;
+}
+const gamehandler = (e) => {
+    let a=e.target.getAttribute('i');
+    let b=e.target.getAttribute('j');
+  if (board[a][b] == -1) {
+    
+    blocks_filled++;
+    board[a][b] = turn;
+    //show in ui..
+    if(turn==1){
+        e.target.innerText='X';
+    }
+    else{
+        e.target.innerText='O';
+    }
+  }
+  if (checker(turn)) {
+    alert(`${turn} won`);
+    reset();
+    return;
+  }
+  turn=1-turn;
+  if (blocks_filled == 9) {
+    alert("tie");
+    reset();
+  }
 };
+
+
+function reset(){
+    for (let i = 0; i < 3; i++) {
+        for(let j=0;j<3;j++){
+        board[i][j] =-1;
+        }
+      }
+      const buttons=document.querySelectorAll('.col');
+      buttons.forEach(function(button){
+          button.innerText='';
+      });
+      blocks_filled=0;turn=1;
+}
 
 //UI
 
@@ -36,3 +107,5 @@ buttons.forEach(function (button) {
 
 //TO DO:
 //minimax algo implement
+
+//its like recursion only...
